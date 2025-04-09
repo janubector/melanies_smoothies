4beb6298ca9d68cdbe2eae6ca91526a7c7ffabbf
janubector <janu.bector@birlasoft.com>
2025-04-09 09:48:42 +0000

    format

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -55,6 +56,6 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
+
 # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
