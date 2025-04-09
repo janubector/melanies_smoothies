0f2f79d7c085569d339f44379fa3a420ac1cd31b
janubector <janu.bector@birlasoft.com>
2025-04-09 07:26:03 +0000

    formatted

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,8 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-# New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # Write directly to the app
